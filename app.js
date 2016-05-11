885d8ec1c1f1e7ee4c8d1d1461b6285e9ad149ec
Sachin C <sachin1245@hotmail.com>
2016-05-11 10:44:42 +0530

    add app id to the api

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ weatherApp.config(function($routeProvider){
 
 weatherApp.service('cityService',function(){
     
-    this.city = " Bangalore ";
+    this.city = "  ";
 })
 
 //Contollers
@@ -42,6 +42,7 @@ weatherApp.controller('homeController',['$scope','cityService',function($scope,c
        
         cityService.city = $scope.city;
     });
+    
 }]);
 
 weatherApp.controller('forecastController',['$scope','$resource','$routeParams','cityService',function($scope, $resource,$routeParams,cityService){
@@ -49,6 +50,8 @@ weatherApp.controller('forecastController',['$scope','$resource','$routeParams',
   $scope.city = cityService.city;
     
   $scope.days = $routeParams.days || '2';
+
+  $scope.appid = "1c3602f494b221b4d49fa7985cbc8636" ;
     
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
         callback: "JSON_CALLBACK"
@@ -58,7 +61,7 @@ weatherApp.controller('forecastController',['$scope','$resource','$routeParams',
       }
   );  
     
-    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city , cnt:$scope.days });
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city , cnt:$scope.days , appid: $scope.appid});
     
     $scope.convertToDegree = function(degC) {
         
